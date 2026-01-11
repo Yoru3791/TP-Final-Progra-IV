3e0a7645ea207c008fd3f5cffd3d9ac11dea1fb4
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2026-01-11 17:08:32 -0300

    update - reclamo-card
    Se agregan las opciones para el uso del admin

--- src/app/components/cards/reclamo-card/reclamo-card.ts
+++ src/app/components/cards/reclamo-card/reclamo-card.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Reclamo } from '../../../model/reclamo-response.model';
 import { CategoriaReclamoLabel } from '../../../constants/categoriaReclamo-labels.const';
 import { EstadoReclamoHelper } from '../../../constants/estadoReclamo-labels.const';
@@ -13,7 +13,12 @@ import { CommonModule } from '@angular/common';
 export class ReclamoCardComponent {
   @Input({ required: true }) reclamo!: Reclamo;
   @Input() isAdmin = false; 
+  @Output() gestionar = new EventEmitter<Reclamo>(); //Si admin toco para gestionar
 
   estadoHelper = EstadoReclamoHelper;
   categoriaLabel = CategoriaReclamoLabel;
+
+  onGestionarClick() {
+    this.gestionar.emit(this.reclamo);
+  }
 }
