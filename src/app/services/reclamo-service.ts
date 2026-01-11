e344c8d49f4cdd4f1fb6938cccea0e9f785ed37e
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2026-01-11 17:12:55 -0300

    Update reclamo-service.ts
    Se agregan rutas para el uso de adminPage

--- src/app/services/reclamo-service.ts
+++ src/app/services/reclamo-service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { ReclamoRequest } from '../model/reclamo-request.model';
 import { Observable } from 'rxjs';
 import { Reclamo } from '../model/reclamo-response.model';
+import { EstadoReclamo } from '../enums/estadoReclamo.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,7 @@ import { Reclamo } from '../model/reclamo-response.model';
 export class ReclamoService {
   private apiUrl = 'http://localhost:8080/api/public/reclamos';
   private apiUrlCliente = 'http://localhost:8080/api/cliente/reclamos';
+  private apiUrlAdmin = 'http://localhost:8080/api/admin/reclamos';
 
   constructor(private http: HttpClient) {}
 
@@ -19,5 +21,15 @@ export class ReclamoService {
 
   getMisReclamos() {
     return this.http.get<Reclamo[]>(`${this.apiUrlCliente}`);
+  }
+
+  // === METODOS ADMIN ===
+  getAllReclamos() {
+    return this.http.get<Reclamo[]>(`${this.apiUrlAdmin}`);
+  }
+
+  actualizarEstado(id: number, nuevoEstado: EstadoReclamo, respuestaAdmin: string) {
+    const body = { nuevoEstado, respuestaAdmin };
+    return this.http.put(`${this.apiUrlAdmin}/id/${id}/estado`, body);
   }
 }
