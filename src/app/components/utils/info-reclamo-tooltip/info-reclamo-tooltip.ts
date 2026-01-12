294665c0e5244ccb8be427d3d130c1c743c1ebf9
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2026-01-11 23:55:45 -0300

    feature - infoReclamoToolTip
    Hover desplegable que muestra informacion de los diferentes estados que puede tener un reclamo o ticket

---
+++ src/app/components/utils/info-reclamo-tooltip/info-reclamo-tooltip.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-info-reclamo-tooltip',
+  imports: [CommonModule],
+  templateUrl: './info-reclamo-tooltip.html',
+  styleUrl: './info-reclamo-tooltip.css'
+})
+export class InfoReclamoTooltipComponent {}
