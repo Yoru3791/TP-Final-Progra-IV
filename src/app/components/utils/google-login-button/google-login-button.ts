cf52eba477f5860ed8118426952898594bac0087
Emiliano Daverio <emilianodaverio@gmail.com>
2026-01-25 14:08:21 -0300

    update manejo snackbar en google auth

--- src/app/components/utils/google-login-button/google-login-button.ts
+++ src/app/components/utils/google-login-button/google-login-button.ts
@@ -4,14 +4,16 @@ import { AuthService } from '../../../services/auth-service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { LoginResponse } from '../../../model/login-response.model';
 import { environment } from '../../../environments/environment';
+import { Snackbar } from '../../modals/snackbar/snackbar';
+import { SnackbarData } from '../../../model/snackbar-data.model';
 
 declare var google: any;
 
 @Component({
   selector: 'app-google-login-button',
-  imports: [], 
+  imports: [],
   templateUrl: './google-login-button.html',
-  styleUrl: './google-login-button.css'
+  styleUrl: './google-login-button.css',
 })
 export class GoogleLoginButton implements AfterViewInit {
   private authService = inject(AuthService);
@@ -21,50 +23,65 @@ export class GoogleLoginButton implements AfterViewInit {
   ngAfterViewInit(): void {
     google.accounts.id.initialize({
       client_id: environment.googleClientId,
-      callback: (response: any) => this.handleGoogleCredential(response)
+      callback: (response: any) => this.handleGoogleCredential(response),
     });
 
-    google.accounts.id.renderButton(
-      document.getElementById("google-btn"),
-      { theme: "outline", size: "large", width: "350", text: "signin_with", logo_alignment: "left" } 
-    );
+    google.accounts.id.renderButton(document.getElementById('google-btn'), {
+      theme: 'outline',
+      size: 'large',
+      width: '350',
+      text: 'signin_with',
+      logo_alignment: 'left',
+    });
   }
 
   handleGoogleCredential(response: any) {
     if (response.credential) {
-      console.log(" Token recibido de Google");
+      console.log(' Token recibido de Google');
 
       this.authService.loginGoogle(response.credential).subscribe({
         next: (res: LoginResponse) => {
-          console.log("Backend respondió OK. Token recibido.");
+          console.log('Backend respondió OK. Token recibido.');
 
           this.authService.handleLoginSuccess(res.token, res.usuarioID, true);
 
-          this.snackBar.open('¡Bienvenido! Sesión iniciada con Google.', 'Cerrar', {
-            duration: 3000,
-            verticalPosition: 'bottom'
-          });
-          
+          this.showSnackBar('¡Bienvenido! Sesión iniciada con Google.', 'check_circle');
+
           setTimeout(() => {
-            this.router.navigate(['/home']).then(success => {
-                if (success) {
-                    console.log("4. Navegación exitosa.");
-                } else {
-                    console.error("4. ERROR: La navegación fue bloqueada.");
-                }
+            this.router.navigate(['/home']).then((success) => {
+              if (success) {
+                console.log('4. Navegación exitosa.');
+              } else {
+                console.error('4. ERROR: La navegación fue bloqueada.');
+              }
             });
-          }, 200); 
+          }, 200);
         },
         error: (err) => {
-          console.error("Error en login Google:", err);
+          console.error('Error en login Google:', err);
           let msg = 'Error al iniciar sesión con Google';
-          
-          if (err.status === 403) msg = 'El correo no está registrado. Por favor regístrese primero.';
+
+          if (err.status === 403)
+            msg = 'El correo no está registrado. Por favor regístrese primero.';
           else if (err.status === 401) msg = 'Cuenta deshabilitada.';
 
-          this.snackBar.open(msg, 'Cerrar', { duration: 5000, panelClass: ['snackbar-error'] });
-        }
+          this.showSnackBar(msg, 'error');
+        },
       });
     }
+  }
+
+  private showSnackBar(message: string, iconName: string) {
+    const snackbarData: SnackbarData = {
+      message: message,
+      iconName: iconName,
+    };
+
+    this.snackBar.openFromComponent(Snackbar, {
+      duration: 3000,
+      verticalPosition: 'bottom',
+      panelClass: 'snackbar-panel',
+      data: snackbarData,
+    });
   }
 }
