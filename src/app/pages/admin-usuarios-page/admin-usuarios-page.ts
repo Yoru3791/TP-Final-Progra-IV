91d76eb40e6bfb228f12c913a29b4d51bcf3b7fe
Tomas-Kauand <kauandtomas@gmail.com>
2026-02-03 03:13:09 -0300

    Fix Estilo Formulario Crear Usuario (admin)
    - se arregló el problema de cambios de tamaño al ver/ocultar contraseña

--- src/app/pages/admin-usuarios-page/admin-usuarios-page.ts
+++ src/app/pages/admin-usuarios-page/admin-usuarios-page.ts
@@ -55,6 +55,8 @@ export class AdminUsuariosPage {
     this.dialog
       .open(AdminUserCreateModal, {
         panelClass: 'form-modal',
+        width: '75rem',
+        maxWidth: '95vw',
         autoFocus: false,
         restoreFocus: false,
       })
