da22d0cf8297dc3fbe77e0f87e742bfa69a7e515
Emiliano Daverio <emilianodaverio@gmail.com>
2026-02-21 18:51:57 -0300

    deploy config

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api',
+  production: true,
+  apiUrl: 'https://mi-viandita.onrender.com',
   googleClientId: '402046863326-2eprqa8vml55ig1d8u5p7t2mh8pobmds.apps.googleusercontent.com' 
 };
