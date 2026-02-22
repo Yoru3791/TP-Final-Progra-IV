bdf19905ef57ed5d053095c5f8d1a4c1fef27a26
Emiliano Daverio <emilianodaverio@gmail.com>
2026-02-22 12:58:20 -0300

    fix url constants

--- src/app/constants/api-urls.const.ts
+++ src/app/constants/api-urls.const.ts
@@ -1,11 +1,11 @@
-import { environment } from "../environments/environment";
+import { environment } from '../environments/environment';
 
 export const API_URLS = {
   BASE: environment.apiUrl,
 
-  ADMIN: `${environment.apiUrl}/admin`,
-  DUENO: `${environment.apiUrl}/dueno`,
-  CLIENTE: `${environment.apiUrl}/cliente`,
-  LOGGED: `${environment.apiUrl}/logged`,
-  PUBLIC: `${environment.apiUrl}/public`
+  ADMIN: `${environment.apiUrl}/api/admin`,
+  DUENO: `${environment.apiUrl}/api/dueno`,
+  CLIENTE: `${environment.apiUrl}/api/cliente`,
+  LOGGED: `${environment.apiUrl}/api/logged`,
+  PUBLIC: `${environment.apiUrl}/api/public`,
 };
